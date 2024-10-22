bb73f576c32404e75cd6875131e0a6d30eee0eba
Davidson Marra <davidson.vieira@stone.com.br>
2024-10-21 21:33:36 -0300

    feat: add money path logo svg

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export {default as AppleIcon} from 'src/assets/icons/apple';
 export {default as GoogleIcon} from 'src/assets/icons/google';
+export {default as MoneyPathIcon} from 'src/assets/icons/money-path';
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export {default as Button, ButtonType} from 'src/components/button';
+export {default as Spacer} from 'src/components/spacer';
 export {default as Text, TextType} from 'src/components/text';
 export {default as StorybookView} from 'src/components/storybook-view';
