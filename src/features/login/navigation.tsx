73aa778fcd9b84f3720f7a0e5dc59a95aea93ff6
Davidson Marra <davidson.vieira@stone.com.br>
2024-10-24 23:35:13 -0300

    chore: add react navigation

---
+++ src/features/login/navigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from 'src/features/login/screens/login';
+
+const {Navigator, Screen} = createStackNavigator();
+
+const LoginStack = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        header: () => null,
+      }}>
+      <Screen name="LoginScreen" component={LoginScreen} />
+    </Navigator>
+  );
+};
+
+export default LoginStack;
