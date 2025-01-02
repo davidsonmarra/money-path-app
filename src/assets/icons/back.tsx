8d7d9f68045625d88e9eccc07544a5972ae46d95
davidsonmarra <davidson.vieira@soupilar.com.br>
2025-01-02 17:50:06 -0300

    feat: add back icon

---
+++ src/assets/icons/back.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+interface Props {
+  size?: number;
+  color?: string;
+}
+
+const BackIcon = ({size = 24, color = '#FFF'}: Props) => (
+  <Svg width={size} height={size} fill={color} viewBox="0 -960 960 960">
+    <Path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144z" />
+  </Svg>
+);
+
+export default BackIcon;
