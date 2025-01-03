58cfceb5feb89c2dafc2095929bf3fc340cb62af
davidsonmarra <davidson.vieira@soupilar.com.br>
2025-01-03 14:38:26 -0300

    feat: add bank icons

---
+++ src/assets/icons/bank.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+interface Props {
+  size?: number;
+  color?: string;
+}
+
+const BankIcon = ({size = 24, color = '#FFF'}: Props) => (
+  <Svg width={size} height={size} fill={color} viewBox="0 -960 960 960">
+    <Path d="M200-280v-280h80v280zm240 0v-280h80v280zM80-120v-80h800v80zm600-160v-280h80v280zM80-640v-80l400-200 400 200v80zm178-80h444zm0 0h444L480-830z" />
+  </Svg>
+);
+
+export default BankIcon;
