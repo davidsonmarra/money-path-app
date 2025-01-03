b77897597f412986b6395685ae4f36260be898d7
davidsonmarra <davidson.vieira@soupilar.com.br>
2025-01-03 14:38:51 -0300

    feat: create icon type enum

---
+++ src/assets/icons/types/index.ts
@@ -0,0 +1,13 @@
+export enum IconType {
+  apple = 'apple',
+  back = 'back',
+  bancoDoBrasil = 'bancoDoBrasil',
+  bank = 'bank',
+  google = 'google',
+  inter = 'inter',
+  itau = 'itau',
+  moneyPath = 'moneyPath',
+  nubank = 'nubank',
+  santander = 'santander',
+  wallet = 'wallet',
+}
