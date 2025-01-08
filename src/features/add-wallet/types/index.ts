3720ae7be38d29cc6694adfbe1d291f1f35cc74a
davidsonmarra <davidsonmarra@gmail.com>
2025-01-07 22:12:22 -0300

    fix: set amount as optional

--- src/features/add-wallet/types/index.ts
+++ src/features/add-wallet/types/index.ts
@@ -6,7 +6,7 @@ export interface InstitutionProps {
   color?: string;
   backgroundColor?: string;
   type: 'personal' | 'bank';
-  amount: number;
+  amount?: number;
 }
 
 export type SelectInstitutionSection = {
